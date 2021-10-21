10e723b8e83212f06e9fd142c549f1e470c34cf6
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-20 19:18:19 -0500

    add search bar and reduce images waiter view

--- burguer-queen/src/components/Diary.js
+++ burguer-queen/src/components/Diary.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import { getData } from '../services/get';
 import '../style/Waiter.css';
 
@@ -9,6 +12,7 @@ const Diary = ({
   productsOrder, setProductsOrder, setQtyChange, setSum, sum,
 }) => {
   const [products, setProducts] = useState(null);
+  const [searchValue, setSearchValue] = useState(null);
 
   useEffect(() => {
     let cancel = false;
@@ -45,11 +49,78 @@ const Diary = ({
     </div>
   ));
 
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(2),
+      width: '90%',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#ffffff',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+  const handleChange = (e) => {
+    setSearchValue((e.target.value).toLowerCase());
+  };
+
+  const showFilterProducts = (products) => {
+    if (searchValue) {
+      const productsFilter = products.filter((p) => p.name.toLowerCase().includes(searchValue));
+      return showProducts(productsFilter);
+    }
+    return showProducts(products);
+  };
+
   return (
     <div>
-      {products
-        ? showProducts(products)
-        : <div />}
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          autoFocus="true"
+          placeholder="Buscar…"
+          inputProps={{ 'aria-label': 'search' }}
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </Search>
+      <div className="waiterProductsContainer">
+        {products
+          ? showFilterProducts(products)
+          : <div />}
+      </div>
     </div>
   );
 };
