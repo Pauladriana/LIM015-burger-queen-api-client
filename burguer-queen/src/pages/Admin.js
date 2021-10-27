0c34995b1ea717a885d1db3473826c882240a501
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-27 17:50:49 -0500

    Try to deploy pwa

--- burguer-queen/src/pages/Admin.js
+++ burguer-queen/src/pages/Admin.js
@@ -13,9 +13,9 @@ import { close, redirectToNotFound } from '../helpers/helpers';
 import Users from './Users';
 import Products from './Products';
 import AllOrders from './AllOrders';
-import NewUser from './Newuser';
+import NewUser from './NewUser';
 import EditUser from './EditUser';
-import NewProduct from './Newproduct';
+import NewProduct from './NewProduct';
 import EditProduct from './EditProduct';
 
 const cookies = new Cookies();
