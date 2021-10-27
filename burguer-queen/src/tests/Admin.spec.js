5abccb521fe225eebf96cf6aecb1b6b303b8d7b3
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-27 10:48:10 -0500

    Change sw.js

--- burguer-queen/src/tests/Admin.spec.js
+++ burguer-queen/src/tests/Admin.spec.js
@@ -26,7 +26,7 @@ beforeEach(() => {
 });
 
 describe('Admin component', () => {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjc2MmViZDMwMDZjNWYzOWNhYWVmOSIsImlhdCI6MTYzNTI1ODQyMiwiZXhwIjoxNjM1MzQ0ODIyfQ.I0giRFvqs_gBF0Or_b5k9_KozpU1frWL4LRluQhx_8E';
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjc2MmViZDMwMDZjNWYzOWNhYWVmOSIsImlhdCI6MTYzNTM0OTYxNSwiZXhwIjoxNjM1NDM2MDE1fQ.oQIRQD7jfvadJn-D9CanrN_qrjKwNQHvxsrN_pa8nR8';
   it('Should contain texts', () => {
     const { getByLabelText, getByText } = render(<Admin />);
     expect(getByLabelText('navlogo').textContent).toContain('BQ');
